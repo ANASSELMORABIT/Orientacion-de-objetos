7d3279fef5b769bcf8d59ce78ff87b3773fa9aee
ANASS EL MORABIT <145122956+ANASSELMORABIT@users.noreply.github.com>
2024-04-23 08:46:36 +0200

    Update Cliente.cs

--- Cliente.cs
+++ Cliente.cs
@@ -1,23 +1,28 @@
-
 public class Cliente
 {
-    private string nombre {get; set;}
-    private string dni {get; set;}
-    private double cantidadPaga {get; set;}
-    private double senal {get; set;}
-    public Cliente(string nombre, string dni, double cantidadPaga, double senal){
-        this.nombre = nombre;
-        this.dni = dni;
-        this.cantidadPaga = cantidadPaga;
-        this.senal = senal;
+    private string Nombre { get; set; }
+    private string Dni { get; set; }
+    private double CantidadPaga { get; set; }
+    private double Senal { get; set; }
+
+    public Cliente(string nombre, string dni, double cantidadPaga, double senal)
+    {
+        Nombre = nombre;
+        Dni = dni;
+        CantidadPaga = cantidadPaga;
+        Senal = senal;
     }
-    public void ConsultarDatosCliente(){
-        Console.WriteLine("Nombre: " + nombre);
-        Console.WriteLine("DNI: " + dni);
-        Console.WriteLine("Cantidad de paga: " + cantidadPaga);
-        Console.WriteLine("Senal: " + senal);
+
+    public void ConsultarDatosCliente()
+    {
+        Console.WriteLine("Nombre: " + Nombre);
+        Console.WriteLine("DNI: " + Dni);
+        Console.WriteLine("Cantidad de paga: " + CantidadPaga);
+        Console.WriteLine("Senal: " + Senal);
     }
-    public void AgregarCliente(string nombre, string dni, double cantidadPaga, double senal,List<string> listaCliente){
+
+    public void AgregarCliente(string nombre, string dni, double cantidadPaga, double senal, List<string> listaCliente)
+    {
         Console.WriteLine("El cliente que quieres agregar es: ");
         Console.WriteLine("Nombre: " + nombre);
         Console.WriteLine("DNI: " + dni);
@@ -25,7 +30,9 @@ public class Cliente
         Console.WriteLine("Senal: " + senal);
         listaCliente.Add(dni);
     }
-    public void QuitarCliente(string nombre, string dni, double cantidadPaga, double senal,List<string> listaCliente){
+
+    public void QuitarCliente(string nombre, string dni, double cantidadPaga, double senal, List<string> listaCliente)
+    {
         Console.WriteLine("El cliente que quieres quitar es: ");
         Console.WriteLine("Nombre: " + nombre);
         Console.WriteLine("DNI: " + dni);
