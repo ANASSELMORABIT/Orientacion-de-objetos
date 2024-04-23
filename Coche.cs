cc3cce8568d129d01fdb82ad48c51ae64551b140
ANASS EL MORABIT <145122956+ANASSELMORABIT@users.noreply.github.com>
2024-04-23 08:45:40 +0200

    Update Coche.cs

--- Coche.cs
+++ Coche.cs
@@ -1,39 +1,38 @@
-
 public class Coche{
-    private string modelo {get; set;}
-    private string marca {get; set;}
-    private int kilometros {get; set;}
-    private int anoFabricacion {get; set;}
-    private int anoMatriculacion {get; set;}
-    private string fechaCompra {get; set;}
-    private double precioCompra {get; set;}
-    private bool probado {get; set;}
+    private string Modelo {get; set;}
+    private string Marca {get; set;}
+    private int Kilometros {get; set;}
+    private int AnoFabricacion {get; set;}
+    private int AnoMatriculacion {get; set;}
+    private string FechaCompra {get; set;}
+    private double PrecioCompra {get; set;}
+    private bool Probado {get; set;}
     public Coche(string modelo, string marca, int kilometros, int anoFabricacion, int anoMatriculacion, string fechaCompra, double precioCompra, bool probado){
-        this.modelo = modelo;
-        this.marca = marca;
-        this.kilometros = kilometros;
-        this.anoFabricacion = anoFabricacion;
-        this.anoMatriculacion = anoMatriculacion;
-        this.fechaCompra = fechaCompra;
-        this.precioCompra = precioCompra;
-        this.probado = probado;
+        Modelo = modelo;
+        Marca = marca;
+        Kilometros = kilometros;
+        AnoFabricacion = anoFabricacion;
+        AnoMatriculacion = anoMatriculacion;
+        FechaCompra = fechaCompra;
+        PrecioCompra = precioCompra;
+        Probado = probado;
     }
-    public double calcularPrecioVenta(double precioCompra){
+    public double CalcularPrecioVenta(double precioCompra){
         return precioCompra * 0.3;
     }
-    public double consultarPrecioCompra(){
-        return precioCompra;
+    public double ConsultarPrecioCompra(){
+        return PrecioCompra;
     }
-    public void consultarDatosCoche(){
-        Console.WriteLine("Modelo: " + modelo);
-        Console.WriteLine("Marca: " + marca);
-        Console.WriteLine("Kilómetros: " + kilometros);
-        Console.WriteLine("Ano de fabricación: " + anoFabricacion);
-        Console.WriteLine("Ano de matriculación: " + anoMatriculacion);
-        Console.WriteLine("Fecha de compra: " + fechaCompra);
-        Console.WriteLine("Probado: " + probado);
-        Console.WriteLine("Precio de compra: " + precioCompra);
-        Console.WriteLine("Precio de venta: " + calcularPrecioVenta(precioCompra));
+    public void ConsultarDatosCoche(){
+        Console.WriteLine("Modelo: " + Modelo);
+        Console.WriteLine("Marca: " + Marca);
+        Console.WriteLine("Kilómetros: " + Kilometros);
+        Console.WriteLine("Año de fabricación: " + AnoFabricacion);
+        Console.WriteLine("Año de matriculación: " + AnoMatriculacion);
+        Console.WriteLine("Fecha de compra: " + FechaCompra);
+        Console.WriteLine("Probado: " + Probado);
+        Console.WriteLine("Precio de compra: " + PrecioCompra);
+        Console.WriteLine("Precio de venta: " + CalcularPrecioVenta(PrecioCompra));
         Console.WriteLine("---------------------------------");
     }
 }
