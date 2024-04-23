17f26f01d21aa691c98ece579c983a3f41e2d9d1
ANASS EL MORABIT <145122956+ANASSELMORABIT@users.noreply.github.com>
2024-04-23 08:46:00 +0200

    Update Proveedores.cs

--- Proveedores.cs
+++ Proveedores.cs
@@ -1,15 +1,15 @@
 
 public class Proveedor
 {
-    private string nombre {get; set;}
-    private string dni {get; set;}
+    private string Nombre {get; set;}
+    private string Dni {get; set;}
     public Proveedor(string nombre, string dni){
-        this.nombre = nombre;
-        this.dni = dni;
+        Nombre = nombre;
+        Dni = dni;
     }
     public void ConsultarDatosProveedor(){
-        Console.WriteLine("Nombre: " + nombre);
-        Console.WriteLine("DNI: " + dni);
+        Console.WriteLine("Nombre: " + Nombre);
+        Console.WriteLine("DNI: " + Dni);
     }
     public void AgregarProveedor(string nombre, string dni,List<string> listaProveedor){
         Console.WriteLine("EL proveedor que quieres agregar es: "); 
